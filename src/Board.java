07bc3db10d9872a0e984a8793495b5d464821cd7
zincalex <92792074+zincalex@users.noreply.github.com>
2021-12-02 19:11:46 +0100

    Huge Changes : -SearchNode merged in Board.java -generateSons changed -Deleted Printing results

--- src/Board.java
+++ src/Board.java
@@ -3,77 +3,59 @@
 import java.lang.Math;
 
 public class Board {
-    private int[][] board;
+    private int[][] matrix;
     private int n;
     private int row0, col0;
+    private int moves;
+    private Board parent;
+    private int priority;
 
-    public Board(int[][] tiles) {
-        board = tiles;
+    //costructor + manhattan distance
+    public Board(int[][] tiles, int m, Board p) {
         n = tiles.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if(board[i][j] == 0) {
-                    row0 = i;
-                    col0 = j;
-                }
-            } 
-        }
-    }
-    //copy constructor
-    public Board(Board b) {
-        n = b.n;
-        board = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = b.board[i][j];
-            }
-        } 
-        row0 = b.row0;
-        col0 = b.col0;
-    }
-
-    //looks if a matrix is equal to the inital board
-    public boolean isInitial(String init) {
-        String questa = toString();
-        if(questa.compareTo(init) == 0) {
-            return true;
-        }
-        return false;
-    }
-
-    // string representation of this board O(n)
-    public String toString() {
-        String representation = "";
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                representation += board[i][j] + " ";
-            } 
-        }
-        return representation;
-    }
-
-    // sum of Manhattan distances between tiles and goal
-    public int manhattan() {
+        moves = m;
+        parent = p;
         int manh = 0;
         int row = 0;
         int col = 0;
+        matrix = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(board[i][j] == 0) continue;
-                row = (board[i][j] - 1) / n;
-                col = (board[i][j] - 1) % n;
+                matrix[i][j] = tiles[i][j];
+                if(tiles[i][j] == 0) {
+                    row0 = i;
+                    col0 = j;
+                    continue;
+                }
+                row = (matrix[i][j] - 1) / n;
+                col = (matrix[i][j] - 1) % n;
                 manh += Math.abs(row - i) + Math.abs(col - j);
             } 
         }
-        return manh;
-    } 
+        priority = manh + moves;
+    }
+
+    //looks if a matrix is equal to the inital board
+    public boolean isInitial(String init) { return toString().equals(init); }
+
+    // string representation of this board O(n)
+    public String toString() {
+        String rapresentation = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rapresentation += matrix[i][j] + " ";
+            } 
+        }
+        return rapresentation;
+    }
+
     // number of tiles out of place
     public int hamming() {
         int ham = 0;
         int pos = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(board[i][j] != pos) 
+                if(matrix[i][j] != pos) 
                     ham++;
                 pos++;
             } 
@@ -83,14 +65,75 @@ public class Board {
 
     // Swap element at (row, col) with the 0 cordinates
     public void swap0(int row, int col) {
-        int temp = board[row][col];
-        board[row][col] = 0;
-        board[row0][col0] = temp;
+        int temp = matrix[row][col];
+        matrix[row][col] = 0;
+        matrix[row0][col0] = temp;
         row0 = row;
         col0 = col;
     }
     
+    public int[][] getBoard() { return matrix; }
+    public int getMoves() { return moves; }
+    public int getPriority() { return priority; }
+    public Board getParent() { return parent; }
     public int get0Row() { return row0; }
     public int get0Col() { return col0; }
-    public int getLength() { return n; }
+    public int getLength() { return n; } //TODO pensare di mettere come variabile static globale nel solver per avere accesso qua x tutte le board
+
+    public Board[] generateSons() {
+        byte count = 0;
+        Board b1 = null;
+        Board b2 = null;
+        Board b3 = null;
+        Board b4 = null;
+        if(get0Row() - 1 >= 0) { 
+            b1 = new Board(matrix, moves + 1, this);
+            b1.swap0(get0Row() - 1, get0Col());
+            if(!getParent().toString().equals(b1.toString())) { 
+                count++;
+            } 
+            else b1 = null;
+            
+        }
+        if(get0Row() + 1 < getLength()) {
+            b2 = new Board(matrix, moves + 1, this);
+            b2.swap0(get0Row() + 1, get0Col());
+            if(!getParent().toString().equals(b2.toString())) {
+               count++;
+            }
+            else b2 = null;
+        }
+        if(get0Col() + 1 < getLength()) {
+            b3 = new Board(matrix, moves + 1, this);
+            b3.swap0(get0Row(), get0Col() + 1);
+            if(!getParent().toString().equals(b3.toString())) {
+                count++;
+            }
+            else b3 = null;
+        }
+        if(get0Col() - 1 >= 0){
+            b4 = new Board(matrix, moves + 1, this);
+            b4.swap0(get0Row(), get0Col() - 1);
+            if(!getParent().toString().equals(b4.toString())) {
+                count++;
+            }
+            else b4 = null;
+        }
+        Board[] figli = new Board[count];
+        byte i = 0;
+        if(b1 != null) {
+            figli[i++] = b1;
+        }
+        if(b2 != null) {
+            figli[i++] = b2;
+        }
+        if(b3 != null) {
+            figli[i++] = b3;
+        }
+        if(b4 != null) {
+            figli[i++] = b4;
+        }
+
+        return figli;
+    }
 }
--- src/Solver.java
+++ src/Solver.java
@@ -21,78 +21,15 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Comparator;
-import java.util.Vector;
 //TODO don’t enqueue a neighbor if its board is the same as the board of the previous search node in the game tree.
 //TODO controllare se 2 nodi hanno la stessa prio, quale delle 2 devo guardare?
 //TODO aggiungere un altra priorità
 //TODO vedere e chiedere a Edo se conviene fare tutto in 1 solo oggetto Board
 
 public class Solver {
-
-    private static class SearchNode {
-        private Board b;
-        private int moves;
-        private SearchNode parent;
-        private int priority;
-
-        public SearchNode(Board b, int m, SearchNode p) {
-            this.b = b;
-            moves = m;
-            parent = p;
-            priority = m + b.manhattan();
-        }
-
-        public Board getBoard() { return b; }
-        public int getMoves() { return moves; }
-        public int getPriority() { return priority; }
-        public SearchNode getParent() { return parent; }
-
-        //Problema con generateSons, è come se non li creasse
-        public Vector<SearchNode> generateSons() {
-            Vector<SearchNode> figli = new Vector<SearchNode>();
-            if(b.get0Row() - 1 >= 0) {
-                int nRow = b.get0Row() - 1; 
-                Board b1 = new Board(b);
-                b1.swap0(nRow, b.get0Col());
-                if(!b1.isInitial(inizio) || !getParent().toString().equals(b1.toString())) {
-                    SearchNode s1 = new SearchNode(b1, getMoves() + 1, this);
-                    figli.add(s1);
-                } 
-            }
-            if(b.get0Row() + 1 < b.getLength()) {
-                int nRow = b.get0Row() + 1;
-                Board b2 = new Board(b);
-                b2.swap0(nRow, b.get0Col());
-                if(!b2.isInitial(inizio) || !getParent().toString().equals(b2.toString())) {
-                    SearchNode s2 = new SearchNode(b2, getMoves() + 1, this);
-                    figli.add(s2);
-                }
-            }
-            if(b.get0Col() + 1 < b.getLength()) {
-                int nCol = b.get0Col() + 1;
-                Board b3 = new Board(b);
-                b3.swap0(b.get0Row(), nCol);
-                if(!b3.isInitial(inizio)  || !getParent().toString().equals(b3.toString())) {
-                    SearchNode s3 = new SearchNode(b3, getMoves() + 1, this);
-                    figli.add(s3);
-                }
-            }
-            if(b.get0Col() - 1 >= 0){
-                int nCol = b.get0Col() - 1;
-                Board b4 = new Board(b);
-                b4.swap0(b.get0Row(), nCol);
-                if(!b4.isInitial(inizio)  || !getParent().toString().equals(b4.toString())) {
-                    SearchNode s4 = new SearchNode(b4, getMoves() + 1, this);
-                    figli.add(s4);
-                }
-            }
-            return figli;
-        }
-    }
-
     //This class allows the priority queue to know which node has the priority over another node
-    private static class BoardComparator implements Comparator<SearchNode> {
-        public int compare(SearchNode b1, SearchNode b2) {
+    private static class BoardComparator implements Comparator<Board> {
+        public int compare(Board b1, Board b2) {
             if(b1.getPriority() == b2.getPriority()) return 0;
             else if(b1.getPriority() < b2.getPriority()) return -1;
             return 1;
@@ -102,9 +39,10 @@ public class Solver {
     //Variabile globale per il goal
     public static String goal = "";
     public static String inizio = "";
+    public static int n = 0;
 
     //Method to generate the goal node in string
-    public static void generateGoal(int n) {
+    public static void generateGoal() {
         for(int i = 1; i < n*n; i++) {
             goal += i + " ";
         }
@@ -115,54 +53,49 @@ public class Solver {
     public static void generateRoot(Board root) { inizio = root.toString(); }
 
     //is the SearchNode the goal Node?
-    public static boolean isGoal(SearchNode gamenode) { 
-        return gamenode.getBoard().toString().equals(goal);
+    public static boolean isGoal(Board gameNode) { 
+        return gameNode.toString().equals(goal);
     }
 
     // test client (see below)
     public static void main(String[] args)  throws FileNotFoundException {
         long start = System.nanoTime();
         Scanner in = new Scanner(new FileReader(args[0]));
-        int n = in.nextInt();
-        generateGoal(n);
+        n = in.nextInt();
+        generateGoal();
+        // TODO da modificare for sure
         int[] strra = new int[(n*n)];
         for(int i = 0; i < strra.length; i++) {
             strra[i] = in.nextInt();
         }
         in.close();
-        int[][] matrix = new int[n][n];
+        int[][] m = new int[n][n];
+        int[][] m_avoid_error = new int[n][n];
         int k = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                matrix[i][j] = strra[k++];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                m[i][j] = strra[k++];
+                m_avoid_error[i][j] = 0;
             } 
         }
 
-        //Resolution of the problem
-        Board init_board = new Board(matrix);
-        SearchNode x = new SearchNode(init_board, 0, null); // si potrebbe fare new SearchNode(new Board(matrix), 0, null);
-        generateRoot(init_board);
-        PriorityQueue<SearchNode> q = new PriorityQueue<SearchNode>(new BoardComparator());
-        while(!isGoal(x)) {
-            Vector<SearchNode> sons = x.generateSons();
-            for(int i = 0; i < sons.size(); i++) {
-                q.add(sons.elementAt(i));
+        //Resolution of the problem, TODO aggiungere banca dati, es hash qualcosa, per sapere se un nodo è già presente nel game tree
+        Board avoid_error = new Board(m_avoid_error, -1, null);
+        Board nodo = new Board(m, 0, avoid_error);
+        generateRoot(nodo);
+        PriorityQueue<Board> q = new PriorityQueue<Board>(new BoardComparator());
+        while(!isGoal(nodo)) {
+            Board[] sons = nodo.generateSons();
+            for(int i = 0; i < sons.length; i++) {
+                q.add(sons[i]);
             }
-            x = q.poll(); 
+            nodo = q.poll(); 
         }
 
         //Printing the request, go look the proper PDF in the directory
-        System.out.println(x.getMoves());
-        String[] stampa = new String[x.getMoves() + 1];
-        int indx = x.getMoves(); 
-        stampa[0] = init_board.toString();
-        while(x.getParent() != null) {
-            stampa[indx--] = x.getBoard().toString(); 
-            x = x.getParent();
-        }
-        for(int i = 0; i < stampa.length; i++) {
-            System.out.println(stampa[i]);
-        }
+        System.out.println(nodo.getMoves());
+         
+        //gestire la stampa
         long finish = System.nanoTime();
         System.out.println((double)(finish - start)/1000000000l);
     
